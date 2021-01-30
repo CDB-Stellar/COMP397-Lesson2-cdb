4c0c6033ce2a6a8abe036c764c828801570dd33c
Chloe <chloe_baker@live.com>
2021-01-29 22:55:52 -0500

    Added camera controller

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float mouseSensitivity = 1000f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked; //won't see cursor when running game
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //mouse look code, can look up down left right
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //Time.deltaTime gives the difference between frames - scaling it according to game time
+
+        xRotation -= mouseY; 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //make sure the rotation doesn't go more than 90 degrees
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float movementForce;
+    public float jumpForce;
     public Rigidbody rigidbody;
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +18,56 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Movement axis scale is from -1 to 1. 
-        if (Input.GetAxisRaw("Horizontal") > 0) //(raw means no smoothing, it's either on or off (-1 or 1))
+        if (isGrounded) //can only move if on the ground
         {
-            //move right
-            //rigidbody.AddForce(new Vector3(1f, 0f, 0f) * movementForce); //could write it this way
-            rigidbody.AddForce(Vector3.right * movementForce); //little more convenient 
-            Debug.Log("Moving right");
+            //Movement axis scale is from -1 to 1. 
+            if (Input.GetAxisRaw("Horizontal") > 0) //(raw means no smoothing, it's either on or off (-1 or 1))
+            {
+                //move right
+                //rigidbody.AddForce(new Vector3(1f, 0f, 0f) * movementForce); //could write it this way
+                rigidbody.AddForce(Vector3.right * movementForce); //little more convenient 
+                Debug.Log("Moving right");
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                //move left
+                rigidbody.AddForce(Vector3.left * movementForce);
+                Debug.Log("Moving left");
+            }
+
+            if (Input.GetAxisRaw("Vertical") > 0) //(raw means no smoothing, it's either on or off (-1 or 1))
+            {
+                //move forward
+                rigidbody.AddForce(Vector3.forward * movementForce);
+            }
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                //move back
+                rigidbody.AddForce(Vector3.back * movementForce);
+            }
+
+            if (Input.GetAxisRaw("Jump") > 0)
+            {
+                rigidbody.AddForce(Vector3.up * jumpForce);
+            }
         }
-        if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            //move left
-            rigidbody.AddForce(Vector3.left * movementForce);
-            Debug.Log("Moving left");
-        }
+    }
+
+    private void OnCollisionEnter(Collision other) //dont really need this one
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = false;
     }
 }
